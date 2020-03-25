d04ec58fff215be3cd65a5aa471974b1180f1850
bernicetse <lingtbt2021@student.cis.edu.hk>
2020-03-25 13:47:29 +0800

    BT: started coding for Start Page

--- app/src/main/java/edu/cis/sensational/Controller/Colorize/ColorizeMainActivity.java
+++ app/src/main/java/edu/cis/sensational/Controller/Colorize/ColorizeMainActivity.java
@@ -1,16 +1,30 @@
 package edu.cis.sensational.Controller.Colorize;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import edu.cis.sensational.R;
 
+
 public class ColorizeMainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colorize_main);
+
     }
+
+
+
+
 }
