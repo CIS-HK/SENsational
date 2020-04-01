47f2a0ab7f2bcedd7fa6e58a447df2efe5e28f92
bernicetse <lingtbt2021@student.cis.edu.hk>
2020-04-01 21:42:59 +0800

    BT: end page layout and buttons present; worked on main page code

--- app/src/main/java/edu/cis/sensational/Controller/Colorize/ColorizeMainActivity.java
+++ app/src/main/java/edu/cis/sensational/Controller/Colorize/ColorizeMainActivity.java
@@ -8,10 +8,14 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
+import edu.cis.sensational.Model.Colorize.GameConstants;
 import edu.cis.sensational.R;
 
 
@@ -19,7 +23,9 @@ public class ColorizeMainActivity extends AppCompatActivity {
 
     Button answerOne, answerTwo, quitButton;
     TextView timeLabel, scoreLabel, colorWord;
-    ArrayList colorwords;
+    ImageView backgroundColor;
+    ArrayList<String> colorWords;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,9 +38,12 @@ public class ColorizeMainActivity extends AppCompatActivity {
         timeLabel = findViewById(R.id.timeLabel);
         scoreLabel = findViewById(R.id.scoreLabel);
         colorWord = findViewById(R.id.colorWord);
+        backgroundColor = findViewById(R.id.backgroundColor);
+        colorWords = new ArrayList<String>();
 
         setUpButtons();
-
+        addColors();
+        play();
     }
     public void setUpButtons()
     {
@@ -45,6 +54,35 @@ public class ColorizeMainActivity extends AppCompatActivity {
             }
         });
 
+
     }
+    public void addColors()
+    {
+        colorWords.add("RED");
+        colorWords.add("ORANGE");
+        colorWords.add("YELLOW");
+        colorWords.add("GREEN");
+        colorWords.add("BLUE");
+        colorWords.add("PURPLE");
+    }
+
+    public void play()
+    {
+        //generate random index from colorarraylist
+        int index = new Random().nextInt(colorWords.size());
+        //set text to random color
+        colorWord.setText(colorWords.get(index));
+
+        //need to figure out how to set text on buttons randomly
+        answerOne.setText(colorWords.get(index));
+
+        //need to set condition if "correct" button is clicked, if so, score ++
+
+    }
+
+
+
+
+
 
 }
