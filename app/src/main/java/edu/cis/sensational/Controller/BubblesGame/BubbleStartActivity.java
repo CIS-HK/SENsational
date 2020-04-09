1d3e4e51abb40086270a7d8f2b1d558cd501b338
SamvittiSharma <samvitti.vs@gmail.com>
2020-04-09 13:21:45 +0800

    Worked on bubbles game

--- app/src/main/java/edu/cis/sensational/Controller/BubblesGame/BubbleStartActivity.java
+++ app/src/main/java/edu/cis/sensational/Controller/BubblesGame/BubbleStartActivity.java
@@ -6,23 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import edu.cis.sensational.R;
 
 public class BubbleStartActivity extends AppCompatActivity {
 
     private Button play;
+    private Button instructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubble_start);
         play = findViewById(R.id.playButton);
+        instructions = findViewById(R.id.instructionsButton);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(BubbleStartActivity.this, BubblesMiddleActivity.class);
+                startActivity(intent);
+            }
+        });
+        instructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BubbleStartActivity.this, BubblesInstructionActivity.class);
                 startActivity(intent);
             }
         });
