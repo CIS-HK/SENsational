1570153f312fb46a8f6673ade864064b27f97a05
SamvittiSharma <samvitti.vs@gmail.com>
2020-04-09 21:49:26 +0800

    Worked on bubbles game

--- app/src/main/java/edu/cis/sensational/Controller/BubblesGame/BubbleStartActivity.java
+++ app/src/main/java/edu/cis/sensational/Controller/BubblesGame/BubbleStartActivity.java
@@ -5,17 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import edu.cis.sensational.R;
 
-public class BubbleStartActivity extends AppCompatActivity {
+public class BubbleStartActivity extends AppCompatActivity
+{
 
     private Button play;
     private Button instructions;
+    private Spinner spinner;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubble_start);
         play = findViewById(R.id.playButton);
@@ -23,7 +28,9 @@ public class BubbleStartActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String mode = spinner.getSelectedItem().toString();
                 Intent intent = new Intent(BubbleStartActivity.this, BubblesMiddleActivity.class);
+                intent.putExtra("mode", mode);
                 startActivity(intent);
             }
         });
@@ -34,5 +41,10 @@ public class BubbleStartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(BubbleStartActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.modes));
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
     }
 }
