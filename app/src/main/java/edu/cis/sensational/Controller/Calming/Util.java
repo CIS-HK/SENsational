b77cf10dec3670059f18b19430a37a2abb5366d9
malinleven <malinel2021@student.cis.edu.hk>
2020-05-01 12:37:59 +0800

    ML fixed the duration of circle

--- app/src/main/java/edu/cis/sensational/Controller/Calming/Util.java
+++ app/src/main/java/edu/cis/sensational/Controller/Calming/Util.java
@@ -21,18 +21,18 @@ public final class Util
         //Creating a new final animation set that will be used on the circle
         final AnimationSet animSet = new AnimationSet(true);
 
-        //Getting the grow animation from anim file and setting the duration to 4 seconds, and
+        //Getting the grow animation from anim file and setting the duration to 3 seconds, and
         //adding it to the animation set for the circle
         Animation grow = AnimationUtils.loadAnimation(context, R.anim.circleanimation2);
-        grow.setDuration(4000);
+        grow.setDuration(3000);
         animSet.addAnimation(grow);
 
-        //Getting the shrink animation from anim file and setting the duration to 4 seconds, and
-        //the start offset to 8 seconds so there will be a 4 second stop before shrink starts and
+        //Getting the shrink animation from anim file and setting the duration to 3 seconds, and
+        //the start offset to 5 seconds so there will be a 2 second stop before shrink starts and
         //adding it to the animation set for the circle
         Animation shrink = AnimationUtils.loadAnimation(context, R.anim.circleanimation);
-        shrink.setDuration(4000);
-        shrink.setStartOffset(8000);
+        shrink.setDuration(3000);
+        shrink.setStartOffset(5000);
         animSet.addAnimation(shrink);
 
         //circle starts to perform the animations in animation set
