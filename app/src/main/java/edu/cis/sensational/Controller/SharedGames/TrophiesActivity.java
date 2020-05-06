2f0199d74ce3bcc7ffa0fb14a15edcabb76b426c
SamvittiSharma <samvitti.vs@gmail.com>
2020-05-06 19:35:14 +0800

    Added trophies RecyclerView

--- app/src/main/java/edu/cis/sensational/Controller/SharedGames/TrophiesActivity.java
+++ app/src/main/java/edu/cis/sensational/Controller/SharedGames/TrophiesActivity.java
@@ -1,10 +1,15 @@
 package edu.cis.sensational.Controller.SharedGames;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 import edu.cis.sensational.R;
 
@@ -12,14 +17,42 @@ public class TrophiesActivity extends AppCompatActivity {
     private Button back;
     private RecyclerView allTrophies;
     private RecyclerView collectedTrophies;
+    private ArrayList<String> trophyNames;
+    private ArrayList<Integer> trophyNums;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophies);
 
-        allTrophies = findViewById(R.id.allRecyclerView);
-        collectedTrophies = findViewById(R.id.collectedRecyclerView);
+        back = findViewById(R.id.backB);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TrophiesActivity.this, GamesSharedActivity.class));
+            }
+        });
 
+        allTrophies = findViewById(R.id.trophiesRecyclerView);
+
+        trophyNames = new ArrayList<>();
+        trophyNames.add("red");
+        trophyNames.add("orange");
+        trophyNames.add("yellow");
+        trophyNames.add("green");
+        trophyNames.add("blue");
+        trophyNames.add("purple");
+
+        trophyNums = new ArrayList<>();
+        trophyNums.add(10);
+        trophyNums.add(20);
+        trophyNums.add(50);
+        trophyNums.add(100);
+        trophyNums.add(200);
+        trophyNums.add(400);
+
+        TrophiesAdapter adapter = new TrophiesAdapter(trophyNames, trophyNums);
+        allTrophies.setAdapter(adapter);
+        allTrophies.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/edu/cis/sensational/Controller/SharedGames/TrophiesAdapter.java
+++ app/src/main/java/edu/cis/sensational/Controller/SharedGames/TrophiesAdapter.java
@@ -1,24 +1,43 @@
 package edu.cis.sensational.Controller.SharedGames;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import edu.cis.sensational.R;
+
 public class TrophiesAdapter extends RecyclerView.Adapter<TrophiesViewHolder>{
+    ArrayList<String> trophyNames;
+    ArrayList<Integer> trophyNums;
+
+    public TrophiesAdapter(ArrayList<String> trophyNames, ArrayList<Integer> trophyNums){
+        this.trophyNames = trophyNames;
+        this.trophyNums = trophyNums;
+    }
+
     @NonNull
     @Override
     public TrophiesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_view, parent, false);
+        TrophiesViewHolder holder = new TrophiesViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull TrophiesViewHolder holder, int position) {
-
+        String trophy = trophyNames.get(position) + " trophy";
+        holder.trophyName.setText(trophy);
+        String num = " " + trophyNums.get(position);
+        holder.numSmileys.setText(num);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return trophyNames.size();
     }
 }
